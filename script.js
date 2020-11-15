0c775cb0f5a13a76053d3bac1cbeec7da3d264b0
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2020-11-15 23:16:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ onload = function () {
     const undo = document.getElementById('undo');
     const clear = document.getElementById('clear');
     const temptext = document.getElementById('temptext');
+
+    textbox.value = "";
+    let text = "";
+    let stack = new Stack();
+
+    textbox.onclick = function () {
+        textbox.selectionStart = textbox.selectionEnd = textbox.value.length;
+    };
+
+    clear.onclick = function () {
+        stack.clear();
+        text = "";
+        textbox.value = "";
+        temptext.innerHTML = "Sequence of operations will be shown here !";
+    };
