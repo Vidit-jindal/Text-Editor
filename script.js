930938f7724f8c2f61ca3cf4aed874152915f4cd
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2020-11-16 22:09:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,18 @@ onload = function () {
         textbox.value = "";
         temptext.innerHTML = "Sequence of operations will be shown here !";
     };
+    
+    textbox.oninput = function(event){
+        // console.log(event);
+        switch(event.inputType){
+            case "insertText":
+                stack.push(0, event.data);
+                break;
+            case "deleteContentBackward":
+                stack.push(1, text[text.length-1]);
+                break;
+        }
+
+        temptext.innerHTML = "On stack "+stack.top()+"<br>"+temptext.innerHTML;
+        text = textbox.value;
+    };
