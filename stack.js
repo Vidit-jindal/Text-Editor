16bdbf88d26ca48e60c910c78325b6da3db6bded
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2020-11-19 22:18:54 +0530

    Update stack.js

--- stack.js
+++ stack.js
@@ -1,3 +1,12 @@
 /**
  * Created by Vidit Jindal on 1/11/20.
  */
+
+export { Stack }
+
+class Stack{
+    constructor(){
+        this.size = 0;
+        this.buffer = 4;
+        this.stack = [];
+    }
